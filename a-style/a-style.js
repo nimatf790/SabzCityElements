b3f040afa8476d59d528c43bc115ac1dcb2b6c25
Omid Hekayati <omid@sabz.city>
2017-09-09 13:12:48 +0430

    Add some New Elements

--- a-style/a-style.js
+++ a-style/a-style.js
@@ -11,6 +11,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+import { Element as PolymerElement } from '../@polymer/polymer/polymer-element.js'
+
 `<custom-style>
   <style is="custom-style">
     html {
